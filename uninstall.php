be85fd0037262fe884038851b2ae12080d52e9eb
Thomas Skerbis <skerbis@klxm.de>
2020-01-20 19:51:45 +0100

    Tabellen löschen

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,11 @@
 <?php
 
+// delete Tables
+rex_sql_table::get(rex::getTable('newsmanager'))
+    ->drop();
+rex_sql_table::get(rex::getTable('newsmanager_categories'))
+    ->drop();
+
 if (is_dir($this->getDataPath())) {
     rex_dir::delete($this->getDataPath());
 }
