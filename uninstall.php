64f4b14c62ef0a444fc483ca892331b64d180600
Thomas Skerbis <skerbis@klxm.de>
2020-01-20 19:58:30 +0100

    DELETE FROM url 1 Addon

--- uninstall.php
+++ uninstall.php
@@ -6,6 +6,13 @@ rex_sql_table::get(rex::getTable('newsmanager'))
 rex_sql_table::get(rex::getTable('newsmanager_categories'))
     ->drop();
 
+$sql = rex_sql::factory();
+$sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE table = "'.rex::getTable('1_xxx_rex_newsmanage').'"');
+$sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE table LIKE "'.rex::getTable('1_xxx_rex_newsmanager_categories').'"');
+
+
+
+
 if (is_dir($this->getDataPath())) {
     rex_dir::delete($this->getDataPath());
 }
