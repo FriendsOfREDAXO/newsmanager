3e9c077f89c066ada93ea3c955e91eaa7c233580
Markus <cobalt60@web.de>
2020-04-01 14:34:36 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -10,7 +10,7 @@ rex_sql_table::get(rex::getTable('newsmanager_categories'))
 
 $sql = rex_sql::factory();
 $sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE `table` = \''.rex::getTable('1_xxx_rex_newsmanager').'\';');
-$sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE `table` LIKE \''.rex::getTable('1_xxx_rex_newsmanager_categories').'\';');
+$sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE `table` = \''.rex::getTable('1_xxx_rex_newsmanager_categories').'\';');
 
 // Delete directory
 
