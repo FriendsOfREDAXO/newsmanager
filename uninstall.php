5e518f0107fc7edad36172a1b16902cfbb98ec2a
Markus <cobalt60@web.de>
2020-04-01 14:21:01 +0200

    Update uninstall.php
    Uninstall Queries korrigiert - liefen auf einen Syntax-Fehler.

--- uninstall.php
+++ uninstall.php
@@ -6,11 +6,13 @@ rex_sql_table::get(rex::getTable('newsmanager'))
 rex_sql_table::get(rex::getTable('newsmanager_categories'))
     ->drop();
 
+// delete Entries in URL Addon
+
 $sql = rex_sql::factory();
-$sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE table = "'.rex::getTable('1_xxx_rex_newsmanager').'"');
-$sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE table LIKE "'.rex::getTable('1_xxx_rex_newsmanager_categories').'"');
+$sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE `table` = \''.rex::getTable('1_xxx_rex_newsmanager').'\';');
+$sql->setQuery('DELETE FROM `'.rex::getTable('url_generate').'` WHERE `table` LIKE \''.rex::getTable('1_xxx_rex_newsmanager_categories').'\';');
 
-
+// Delete directory
 
 
 if (is_dir($this->getDataPath())) {
