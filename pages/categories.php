d94438bae84646004fbae77b57bb5f01e94f4de6
eaCe <webmaster@slaps.de>
2022-10-15 19:05:52 +0200

    get start clang id, fix add category

--- pages/categories.php
+++ pages/categories.php
@@ -4,14 +4,10 @@ $content = "";
 
 $category_id = rex_request('category_id', 'int');
 $func = rex_request('func', 'string');
-$clang_id = rex_request('clang_id', 'int');
+$clang_id = rex_request('clang_id', 'int', rex_clang::getStartId());
 
 $fragment = new rex_fragment();
 
-
-if ($clang_id == "") {
-    $clang_id = 1;
-}
 $success = "";
 $error = "";
 
@@ -42,7 +38,7 @@ if ($func == "") {
     if (count(rex_clang::getAll(1))>1) {
         echo '<div class="rex-nav-btn news-lang-switch"><div class="btn-toolbar"><div class="btn-group pull-right">'.PHP_EOL;
         foreach(rex_clang::getAll(1) as $key => $lang) {
-            
+
             if ($clang_id == $lang->getId()) {
                 $active = 'class="btn btn-clang active"';
             } else {
@@ -52,10 +48,10 @@ if ($func == "") {
         }
         echo '</div></div></div>'.PHP_EOL;
     }
-    
-  
+
+
     $fragment->setVar('title', $this->i18n('categories'), false);
-    
+
     //$clang_id = 1;
 
     $list = rex_list::factory('SELECT pid,id,name,clang_id FROM ' . rex::getTablePrefix() . 'newsmanager_categories WHERE clang_id = '.$clang_id.' ORDER BY name ASC', 25);
@@ -69,12 +65,12 @@ if ($func == "") {
     $list->removeColumn('pid');
     $list->removeColumn('id');
     $list->removeColumn('clang_id');
-    
+
     $list->setColumnLabel('name', 'Name');
     $list->setColumnSortable('name');
     $list->setColumnParams('name', ['clang_id' => $clang_id, 'func' => 'edit', 'category_id' => '###pid###']);
-    
-    
+
+
     $list->addColumn('deleteCategory', '', -1, ['<th></th>', '<td class="rex-table-action">###VALUE###</td>']);
     $list->setColumnParams('deleteCategory', ['clang_id' => $clang_id, 'category_id' => '###id###', 'func' => 'delete']);
     $list->addLinkAttribute('deleteCategory', 'data-confirm', rex_i18n::msg('category_delete_question') . ' ?');
@@ -82,28 +78,28 @@ if ($func == "") {
         $list = $params['list'];
         return $list->getColumnLink('deleteCategory', '<i class="rex-icon rex-icon-delete"></i> ' . rex_i18n::msg('newsmanager_delete'));
     });
-    
+
     $content .= $list->get();
 } elseif ($func == 'add' || $func == 'edit') {
-    
+
     $fragment->setVar('title', $this->i18n('category'), false);
-    
+
     $form = rex_form::factory(rex::getTablePrefix() . 'newsmanager_categories', '', 'pid = ' . $category_id);
-    
+
     $form->setLanguageSupport('id', 'clang_id');
-    
+
     $form->addParam('category_id', $category_id);
     $form->addParam('clang_id', $clang_id);
-   
+
     if ($func == 'edit') {
         $form->setEditMode($func == 'edit');
     }
 
     $field = $form->addTextField('name');
     $field->setLabel(rex_i18n::msg('category_form_name'));
-    
-    $content .= $form->get();   
-    
+
+    $content .= $form->get();
+
     $fragment->setVar('class', 'edit');
 }
 
