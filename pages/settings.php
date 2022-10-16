29051801481a5de1b76ba4b919735f9d630527bb
eaCe <webmaster@slaps.de>
2022-10-16 07:26:36 +0200

    fix settings, update to url v2, declutter settings

--- pages/settings.php
+++ pages/settings.php
@@ -2,75 +2,127 @@
 
 $content = '';
 $buttons = '';
+$profileTable = rex::getTable('url_generator_profile');
 
 if (rex_post('formsubmit', 'string') == '1') {
     $this->setConfig(rex_post('config', [
         ['newsmanager-page-id', 'array[int]'],
     ]));
-    
+
     if (rex_addon::getConfig('newsmanager-page-id')) {
         $id = rex_addon::getConfig('newsmanager-page-id')[0];
-        
+
         if ($id > 0) {
-    
             $article_id = $id;
             $user = rex::getUser()->getLogin();
 
-            if (count(rex_clang::getAll())==1) {      
-                $query = "DELETE FROM `rex_url_generate` WHERE `table` = '1_xxx_rex_newsmanager'";
+            if (count(rex_clang::getAll()) == 1) {
+                $query = "DELETE FROM $profileTable WHERE `table_name` = '1_xxx_rex_newsmanager'";
                 $result = rex_sql::factory()->setQuery($query);
-                $query = "DELETE FROM `rex_url_generate` WHERE `table` LIKE '1_xxx_rex_newsmanager_categories'";
-                $result = rex_sql::factory()->setQuery($query);
-
-                $query = "INSERT INTO `rex_url_generate` (`article_id`, `clang_id`, `url`, `table`, `table_parameters`, `relation_table`, `relation_table_parameters`, `relation_insert`, `createdate`, `createuser`, `updatedate`, `updateuser`)
-            VALUES
-                    ($article_id, 1, '', '1_xxx_rex_newsmanager', '{\"1_xxx_rex_newsmanager_field_1\":\"title\",\"1_xxx_rex_newsmanager_field_2\":\"\",\"1_xxx_rex_newsmanager_field_3\":\"\",\"1_xxx_rex_newsmanager_id\":\"id\",\"1_xxx_rex_newsmanager_clang_id\":\"\",\"1_xxx_rex_newsmanager_restriction_field\":\"status\",\"1_xxx_rex_newsmanager_restriction_operator\":\"=\",\"1_xxx_rex_newsmanager_restriction_value\":\"1\",\"1_xxx_rex_newsmanager_url_param_key\":\"newsmanager\",\"1_xxx_rex_newsmanager_seo_title\":\"title\",\"1_xxx_rex_newsmanager_seo_description\":\"seo_description\",\"1_xxx_rex_newsmanager_seo_image\":\"\",\"1_xxx_rex_newsmanager_sitemap_add\":\"1\",\"1_xxx_rex_newsmanager_sitemap_frequency\":\"daily\",\"1_xxx_rex_newsmanager_sitemap_priority\":\"1.0\",\"1_xxx_rex_newsmanager_sitemap_lastmod\":\"updatedate\",\"1_xxx_rex_newsmanager_path_names\":\"\",\"1_xxx_rex_newsmanager_path_categories\":\"0\",\"1_xxx_rex_newsmanager_relation_field\":\"\"}', '', '[]', 'before', 2017, '$user', 2017, '$user');";
-
-                $result1 = rex_sql::factory()->setQuery($query); 
-
-                $query = "INSERT INTO `rex_url_generate` (`article_id`, `clang_id`, `url`, `table`, `table_parameters`, `relation_table`, `relation_table_parameters`, `relation_insert`, `createdate`, `createuser`, `updatedate`, `updateuser`)
-            VALUES
-                    ($article_id, 1, '', '1_xxx_rex_newsmanager_categories', '{\"1_xxx_rex_newsmanager_categories_field_1\":\"name\",\"1_xxx_rex_newsmanager_categories_field_2\":\"\",\"1_xxx_rex_newsmanager_categories_field_3\":\"\",\"1_xxx_rex_newsmanager_categories_id\":\"id\",\"1_xxx_rex_newsmanager_categories_clang_id\":\"\",\"1_xxx_rex_newsmanager_categories_restriction_field\":\"\",\"1_xxx_rex_newsmanager_categories_restriction_operator\":\"=\",\"1_xxx_rex_newsmanager_categories_restriction_value\":\"\",\"1_xxx_rex_newsmanager_categories_url_param_key\":\"newsmanager_category\",\"1_xxx_rex_newsmanager_categories_seo_title\":\"name\",\"1_xxx_rex_newsmanager_categories_seo_description\":\"\",\"1_xxx_rex_newsmanager_categories_seo_image\":\"\",\"1_xxx_rex_newsmanager_categories_sitemap_add\":\"1\",\"1_xxx_rex_newsmanager_categories_sitemap_frequency\":\"always\",\"1_xxx_rex_newsmanager_categories_sitemap_priority\":\"1.0\",\"1_xxx_rex_newsmanager_categories_sitemap_lastmod\":\"updatedate\",\"1_xxx_rex_newsmanager_categories_path_names\":\"\",\"1_xxx_rex_newsmanager_categories_path_categories\":\"0\",\"1_xxx_rex_newsmanager_categories_relation_field\":\"\"}', '', '[]', 'before', 2017, '$user', 2017, '$user');";
-                $result2 = rex_sql::factory()->setQuery($query);
-            } else {
-                $query = "DELETE FROM `rex_url_generate` WHERE `table` = '1_xxx_rex_newsmanager'";
-                $result = rex_sql::factory()->setQuery($query);
-                $query = "DELETE FROM `rex_url_generate` WHERE `table` LIKE '1_xxx_rex_newsmanager_categories'";
+                $query = "DELETE FROM $profileTable WHERE `table_name` LIKE '1_xxx_rex_newsmanager_categories'";
                 $result = rex_sql::factory()->setQuery($query);
 
-                $query = "INSERT INTO `rex_url_generate` (`article_id`, `clang_id`, `url`, `table`, `table_parameters`, `relation_table`, `relation_table_parameters`, `relation_insert`, `createdate`, `createuser`, `updatedate`, `updateuser`)
-            VALUES
-                    ($article_id, 0, '', '1_xxx_rex_newsmanager', '{\"1_xxx_rex_newsmanager_field_1\":\"title\",\"1_xxx_rex_newsmanager_field_2\":\"\",\"1_xxx_rex_newsmanager_field_3\":\"\",\"1_xxx_rex_newsmanager_id\":\"id\",\"1_xxx_rex_newsmanager_clang_id\":\"clang_id\",\"1_xxx_rex_newsmanager_restriction_field\":\"status\",\"1_xxx_rex_newsmanager_restriction_operator\":\"=\",\"1_xxx_rex_newsmanager_restriction_value\":\"1\",\"1_xxx_rex_newsmanager_url_param_key\":\"newsmanager\",\"1_xxx_rex_newsmanager_seo_title\":\"title\",\"1_xxx_rex_newsmanager_seo_description\":\"seo_description\",\"1_xxx_rex_newsmanager_seo_image\":\"\",\"1_xxx_rex_newsmanager_sitemap_add\":\"1\",\"1_xxx_rex_newsmanager_sitemap_frequency\":\"daily\",\"1_xxx_rex_newsmanager_sitemap_priority\":\"1.0\",\"1_xxx_rex_newsmanager_sitemap_lastmod\":\"updatedate\",\"1_xxx_rex_newsmanager_path_names\":\"\",\"1_xxx_rex_newsmanager_path_categories\":\"0\",\"1_xxx_rex_newsmanager_relation_field\":\"\"}', '', '[]', 'before', 2017, '$user', 2017, '$user');";
+                $newsManagerSql = rex_sql::factory();
+                $newsManagerSql->setTable($profileTable);
+                $newsManagerSql->setValue('namespace', 'newsmanager');
+                $newsManagerSql->setValue('article_id', $article_id);
+                $newsManagerSql->setValue('clang_id', rex_clang::getStartId());
+                $newsManagerSql->setValue('ep_pre_save_called', 0);
+                $newsManagerSql->setValue('table_name', '1_xxx_rex_newsmanager');
+                $newsManagerSql->setValue('table_parameters', '{"column_id":"id","column_clang_id":"clang_id","restriction_1_column":"status","restriction_1_comparison_operator":"=","restriction_1_value":"1","restriction_2_logical_operator":"","restriction_2_column":"","restriction_2_comparison_operator":"=","restriction_2_value":"","restriction_3_logical_operator":"","restriction_3_column":"","restriction_3_comparison_operator":"=","restriction_3_value":"","column_segment_part_1":"title","column_segment_part_2_separator":"\/","column_segment_part_2":"","column_segment_part_3_separator":"\/","column_segment_part_3":"","relation_1_column":"","relation_1_position":"BEFORE","relation_2_column":"","relation_2_position":"BEFORE","relation_3_column":"","relation_3_position":"BEFORE","append_user_paths":"","append_structure_categories":"0","column_seo_title":"title","column_seo_description":"seo_description","column_seo_image":"","sitemap_add":"1","sitemap_frequency":"daily","sitemap_priority":"1.0","column_sitemap_lastmod":"updatedate"}');
+                $newsManagerSql->setValue('relation_1_table_name', '');
+                $newsManagerSql->setValue('relation_1_table_parameters', '[]');
+                $newsManagerSql->setValue('relation_2_table_name', '');
+                $newsManagerSql->setValue('relation_2_table_parameters', '[]');
+                $newsManagerSql->setValue('relation_3_table_name', '');
+                $newsManagerSql->setValue('relation_3_table_parameters', '[]');
+                $newsManagerSql->setValue('createdate', rex_sql::datetime());
+                $newsManagerSql->setValue('updatedate', rex_sql::datetime());
+                $newsManagerSql->setValue('createuser', $user);
+                $newsManagerSql->setValue('updateuser', $user);
+                $newsManagerSql->insert();
 
-                $result = rex_sql::factory()->setQuery($query); 
-
-                $query = "INSERT INTO `rex_url_generate` (`article_id`, `clang_id`, `url`, `table`, `table_parameters`, `relation_table`, `relation_table_parameters`, `relation_insert`, `createdate`, `createuser`, `updatedate`, `updateuser`)
-            VALUES
-                    ($article_id, 0, '', '1_xxx_rex_newsmanager_categories', '{\"1_xxx_rex_newsmanager_categories_field_1\":\"name\",\"1_xxx_rex_newsmanager_categories_field_2\":\"\",\"1_xxx_rex_newsmanager_categories_field_3\":\"\",\"1_xxx_rex_newsmanager_categories_id\":\"id\",\"1_xxx_rex_newsmanager_categories_clang_id\":\"clang_id\",\"1_xxx_rex_newsmanager_categories_restriction_field\":\"\",\"1_xxx_rex_newsmanager_categories_restriction_operator\":\"=\",\"1_xxx_rex_newsmanager_categories_restriction_value\":\"\",\"1_xxx_rex_newsmanager_categories_url_param_key\":\"newsmanager_category\",\"1_xxx_rex_newsmanager_categories_seo_title\":\"name\",\"1_xxx_rex_newsmanager_categories_seo_description\":\"\",\"1_xxx_rex_newsmanager_categories_seo_image\":\"\",\"1_xxx_rex_newsmanager_categories_sitemap_add\":\"1\",\"1_xxx_rex_newsmanager_categories_sitemap_frequency\":\"always\",\"1_xxx_rex_newsmanager_categories_sitemap_priority\":\"1.0\",\"1_xxx_rex_newsmanager_categories_sitemap_lastmod\":\"updatedate\",\"1_xxx_rex_newsmanager_categories_path_names\":\"\",\"1_xxx_rex_newsmanager_categories_path_categories\":\"0\",\"1_xxx_rex_newsmanager_categories_relation_field\":\"\"}', '', '[]', 'before', 2017, '$user', 2017, '$user');";
+                $newsManagerSqlCategories = rex_sql::factory();
+                $newsManagerSqlCategories->setTable($profileTable);
+                $newsManagerSqlCategories->setValue('namespace', 'newsmanager_category');
+                $newsManagerSqlCategories->setValue('article_id', $article_id);
+                $newsManagerSqlCategories->setValue('clang_id', rex_clang::getStartId());
+                $newsManagerSqlCategories->setValue('ep_pre_save_called', 0);
+                $newsManagerSqlCategories->setValue('table_name', '1_xxx_rex_newsmanager_categories');
+                $newsManagerSqlCategories->setValue('table_parameters', '{"column_id":"id","column_clang_id":"clang_id","restriction_1_column":"","restriction_1_comparison_operator":"=","restriction_1_value":"","restriction_2_logical_operator":"","restriction_2_column":"","restriction_2_comparison_operator":"=","restriction_2_value":"","restriction_3_logical_operator":"","restriction_3_column":"","restriction_3_comparison_operator":"=","restriction_3_value":"","column_segment_part_1":"name","column_segment_part_2_separator":"\/","column_segment_part_2":"","column_segment_part_3_separator":"\/","column_segment_part_3":"","relation_1_column":"","relation_1_position":"BEFORE","relation_2_column":"","relation_2_position":"BEFORE","relation_3_column":"","relation_3_position":"BEFORE","append_user_paths":"","append_structure_categories":"0","column_seo_title":"name","column_seo_description":"","column_seo_image":"","sitemap_add":"1","sitemap_frequency":"always","sitemap_priority":"1.0","column_sitemap_lastmod":"updatedate"}');
+                $newsManagerSqlCategories->setValue('relation_1_table_name', '');
+                $newsManagerSqlCategories->setValue('relation_1_table_parameters', '[]');
+                $newsManagerSqlCategories->setValue('relation_2_table_name', '');
+                $newsManagerSqlCategories->setValue('relation_2_table_parameters', '[]');
+                $newsManagerSqlCategories->setValue('relation_3_table_name', '');
+                $newsManagerSqlCategories->setValue('relation_3_table_parameters', '[]');
+                $newsManagerSqlCategories->setValue('createdate', rex_sql::datetime());
+                $newsManagerSqlCategories->setValue('updatedate', rex_sql::datetime());
+                $newsManagerSqlCategories->setValue('createuser', $user);
+                $newsManagerSqlCategories->setValue('updateuser', $user);
+                $newsManagerSqlCategories->insert();
+            }
+            else {
+                $query = "DELETE FROM $profileTable WHERE `table_name` = '1_xxx_rex_newsmanager'";
                 $result = rex_sql::factory()->setQuery($query);
+                $query = "DELETE FROM $profileTable WHERE `table_name` LIKE '1_xxx_rex_newsmanager_categories'";
+                $result = rex_sql::factory()->setQuery($query);
+
+                $newsManagerSql = rex_sql::factory();
+                $newsManagerSql->setTable($profileTable);
+                $newsManagerSql->setValue('namespace', 'newsmanager');
+                $newsManagerSql->setValue('article_id', $article_id);
+                $newsManagerSql->setValue('clang_id', 0);
+                $newsManagerSql->setValue('ep_pre_save_called', 0);
+                $newsManagerSql->setValue('table_name', '1_xxx_rex_newsmanager');
+                $newsManagerSql->setValue('table_parameters', '{"column_id":"id","column_clang_id":"clang_id","restriction_1_column":"status","restriction_1_comparison_operator":"=","restriction_1_value":"1","restriction_2_logical_operator":"","restriction_2_column":"","restriction_2_comparison_operator":"=","restriction_2_value":"","restriction_3_logical_operator":"","restriction_3_column":"","restriction_3_comparison_operator":"=","restriction_3_value":"","column_segment_part_1":"title","column_segment_part_2_separator":"\/","column_segment_part_2":"","column_segment_part_3_separator":"\/","column_segment_part_3":"","relation_1_column":"","relation_1_position":"BEFORE","relation_2_column":"","relation_2_position":"BEFORE","relation_3_column":"","relation_3_position":"BEFORE","append_user_paths":"","append_structure_categories":"0","column_seo_title":"title","column_seo_description":"seo_description","column_seo_image":"","sitemap_add":"1","sitemap_frequency":"daily","sitemap_priority":"1.0","column_sitemap_lastmod":"updatedate"}');
+                $newsManagerSql->setValue('relation_1_table_name', '');
+                $newsManagerSql->setValue('relation_1_table_parameters', '[]');
+                $newsManagerSql->setValue('relation_2_table_name', '');
+                $newsManagerSql->setValue('relation_2_table_parameters', '[]');
+                $newsManagerSql->setValue('relation_3_table_name', '');
+                $newsManagerSql->setValue('relation_3_table_parameters', '[]');
+                $newsManagerSql->setValue('createdate', rex_sql::datetime());
+                $newsManagerSql->setValue('updatedate', rex_sql::datetime());
+                $newsManagerSql->setValue('createuser', $user);
+                $newsManagerSql->setValue('updateuser', $user);
+                $newsManagerSql->insert();
+
+                $newsManagerSqlCategories = rex_sql::factory();
+                $newsManagerSqlCategories->setTable($profileTable);
+                $newsManagerSqlCategories->setValue('namespace', 'newsmanager_category');
+                $newsManagerSqlCategories->setValue('article_id', $article_id);
+                $newsManagerSqlCategories->setValue('clang_id', 0);
+                $newsManagerSqlCategories->setValue('ep_pre_save_called', 0);
+                $newsManagerSqlCategories->setValue('table_name', '1_xxx_rex_newsmanager_categories');
+                $newsManagerSqlCategories->setValue('table_parameters', '{"column_id":"id","column_clang_id":"clang_id","restriction_1_column":"","restriction_1_comparison_operator":"=","restriction_1_value":"","restriction_2_logical_operator":"","restriction_2_column":"","restriction_2_comparison_operator":"=","restriction_2_value":"","restriction_3_logical_operator":"","restriction_3_column":"","restriction_3_comparison_operator":"=","restriction_3_value":"","column_segment_part_1":"name","column_segment_part_2_separator":"\/","column_segment_part_2":"","column_segment_part_3_separator":"\/","column_segment_part_3":"","relation_1_column":"","relation_1_position":"BEFORE","relation_2_column":"","relation_2_position":"BEFORE","relation_3_column":"","relation_3_position":"BEFORE","append_user_paths":"","append_structure_categories":"0","column_seo_title":"name","column_seo_description":"","column_seo_image":"","sitemap_add":"1","sitemap_frequency":"always","sitemap_priority":"1.0","column_sitemap_lastmod":"updatedate"}');
+                $newsManagerSqlCategories->setValue('relation_1_table_name', '');
+                $newsManagerSqlCategories->setValue('relation_1_table_parameters', '[]');
+                $newsManagerSqlCategories->setValue('relation_2_table_name', '');
+                $newsManagerSqlCategories->setValue('relation_2_table_parameters', '[]');
+                $newsManagerSqlCategories->setValue('relation_3_table_name', '');
+                $newsManagerSqlCategories->setValue('relation_3_table_parameters', '[]');
+                $newsManagerSqlCategories->setValue('createdate', rex_sql::datetime());
+                $newsManagerSqlCategories->setValue('updatedate', rex_sql::datetime());
+                $newsManagerSqlCategories->setValue('createuser', $user);
+                $newsManagerSqlCategories->setValue('updateuser', $user);
+                $newsManagerSqlCategories->insert();
             }
 
-           
             Url\Url::resolveCurrent();
-
         }
     }
     if (rex_addon::get('redactor2')->isAvailable()) {
-       
         if (!redactor2::profileExists('newsmanager')) {
-            redactor2::insertProfile('newsmanager', 'Angelegt durch das News-Manager Addon', '300', '800', 'relative', '0', '0', '0', '1','groupheading[2|3|4|5|6],paragraph,bold, italic, underline,deleted, sub, sup,  unorderedlist, orderedlist,video,media,grouplink[email|external|internal|media|telephone], cleaner,horizontalrule,source,fullscreen');
+            redactor2::insertProfile('newsmanager', 'Angelegt durch das News-Manager Addon', '300', '800', 'relative', '0', '0', '0', '1', 'groupheading[2|3|4|5|6],paragraph,bold, italic, underline,deleted, sub, sup,  unorderedlist, orderedlist,video,media,grouplink[email|external|internal|media|telephone], cleaner,horizontalrule,source,fullscreen');
             echo rex_view::success('Das nötige Redactor2-Profil wurde angelegt');
         }
-        
-    } else {
-        
-        echo rex_view::info($this->i18n('settings_error_redactor2'));
-        
     }
-    
+    else {
+        echo rex_view::info($this->i18n('settings_error_redactor2'));
+    }
 
     echo rex_view::success($this->i18n('settings_saved'));
-
 }
 
 $content .= '<fieldset><legend>' . $this->i18n('settings_category') . '</legend>';
