cda16a7274818fe9e2efe9210be4996e0f1ddc96
eaCe <webmaster@slaps.de>
2022-10-20 08:34:31 +0200

    fix: comments plugin

--- plugins/comments/lib/Comment.php
+++ plugins/comments/lib/Comment.php
@@ -1,8 +1,7 @@
 <?php
 
-class Comment 
+class Comment
 {
-    
     private $id;
     private $re_article_pid;
     private $comment;
@@ -10,8 +9,9 @@ class Comment
     private $email;
     private $createdate;
     private $status;
-    
-    public function __construct($result) {
+
+    public function __construct($result)
+    {
         $this->id = $result->getValue('id');
         $this->re_article_id = $result->getValue('re_article_pid');
         $this->comment = $result->getValue('comment');
@@ -19,63 +19,75 @@ class Comment
         $this->email = $result->getValue('email');
         $this->createdate = $result->getValue('createdate');
         $this->status = $result->getValue('status');
-        
     }
-    
-    public function getId() {
+
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getRe_article_pid() {
+    public function getRe_article_pid()
+    {
         return $this->re_article_pid;
     }
 
-    public function getComment() {
+    public function getComment()
+    {
         return $this->comment;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
 
-    public function getEmail() {
+    public function getEmail()
+    {
         return $this->email;
     }
 
-    public function getCreatedate() {
+    public function getCreatedate()
+    {
         return $this->createdate;
     }
 
-    public function getStatus() {
+    public function getStatus()
+    {
         return $this->status;
     }
 
-    public function setId($id) {
+    public function setId($id)
+    {
         $this->id = $id;
     }
 
-    public function setRe_article_pid($re_article_pid) {
+    public function setRe_article_pid($re_article_pid)
+    {
         $this->re_article_pid = $re_article_pid;
     }
 
-    public function setComment($comment) {
+    public function setComment($comment)
+    {
         $this->comment = $comment;
     }
 
-    public function setName($name) {
+    public function setName($name)
+    {
         $this->name = $name;
     }
 
-    public function setEmail($email) {
+    public function setEmail($email)
+    {
         $this->email = $email;
     }
 
-    public function setCreatedate($createdate) {
+    public function setCreatedate($createdate)
+    {
         $this->createdate = $createdate;
     }
 
-    public function setStatus($status) {
+    public function setStatus($status)
+    {
         $this->status = $status;
     }
-
 }
--- plugins/comments/pages/index.php
+++ plugins/comments/pages/index.php
@@ -8,7 +8,7 @@ $content = '';
 $fragment = new rex_fragment();
 
 if ($func == 'delete' && $id > 0) {
-    
+
     $sql = rex_sql::factory();
     //$sql->setDebug();
     $sql->setTable(rex::getTablePrefix() . 'newsmanager_comments');
@@ -17,20 +17,22 @@ if ($func == 'delete' && $id > 0) {
     try {
         $sql->delete();
         $success = rex_i18n::msg('newsmanager_deleted');
-    } catch (rex_sql_exception $e) {
+    }
+    catch (rex_sql_exception $e) {
         $error = $sql->getError();
     }
     $func = '';
 }
 
 if ($func == 'status' && $id > 0) {
-    
+
     $sql = rex_sql::factory();
-    
+
     try {
-        $sql->setQuery('UPDATE '. rex::getTablePrefix() . 'newsmanager_comments SET status = IF(status=1, 0, 1) WHERE id = '.$id.'');
-        
-    } catch (rex_sql_exception $e) {
+        $sql->setQuery('UPDATE ' . rex::getTablePrefix() . 'newsmanager_comments SET status = IF(status=1, 0, 1) WHERE id = ' . $id . '');
+
+    }
+    catch (rex_sql_exception $e) {
         $error = $sql->getError();
     }
 
@@ -39,104 +41,108 @@ if ($func == 'status' && $id > 0) {
 
 if ($func == "") {
     $fragment->setVar('title', $this->i18n('comments_title'), false);
-    
+
     $list = rex_list::factory(''
-            . 'SELECT * '
-            . 'FROM ' . rex::getTablePrefix() . 'newsmanager_comments '
-            . 'ORDER BY createdate DESC', 25);
-    
+        . 'SELECT * '
+        . 'FROM ' . rex::getTablePrefix() . 'newsmanager_comments '
+        . 'ORDER BY createdate DESC', 25);
+
     $list->addTableAttribute('class', 'table-striped');
-    
+
     $tdIcon = '<i class="rex-icon fa-comment"></i>';
     $thIcon = '';
     $list->addColumn($thIcon, $tdIcon, 0, ['<th class="rex-table-icon">###VALUE###</th>', '<td class="rex-table-icon">###VALUE###</td>']);
     $list->setColumnParams($thIcon, ['func' => 'edit', 'id' => '###id###']);
-    
+
     $list->removeColumn('id');
     $list->removeColumn('re_article_pid');
     $list->removeColumn('email');
-    
+
     $list->setColumnLabel('comment', rex_i18n::msg("comment_tableheader_comment"));
     $list->setColumnSortable('comment');
-    $list->setColumnFormat('comment', 'custom', function($params) {
+    $list->setColumnFormat('comment', 'custom', function ($params)
+    {
         $list = $params['list'];
         // Kommentar kürzen und Punkte dranhängen 
         if (strlen($params['value']) > 70) {
             $comment = substr(preg_replace("/[^ ]*$/", '', substr($params['value'], 0, 70)), 0, -1) . '...';
-        } else {
+        }
+        else {
             $comment = $params['value'];
         }
-        return $list->getColumnLink('comment', $comment , ['func' => 'edit', 'id' => '###id###']);
+        return $list->getColumnLink('comment', $comment, ['func' => 'edit', 'id' => '###id###']);
     });
-   
+
     $list->setColumnLabel('name', rex_i18n::msg("comment_tableheader_name"));
     $list->setColumnSortable('name');
-    
+
     $list->setColumnLabel('createdate', rex_i18n::msg('comment_tableheader_createdate'));
     $list->setColumnSortable('createdate');
     $list->setColumnFormat('createdate', 'date', 'd.m.Y, G:i');
-    
-    $list->setColumnFormat('status', 'custom',
-        create_function(
-            '$params',
-            'global $I18N;
-$list = $params["list"];
-if ($list->getValue("status") == 1)
-$str = "<a class=\"no-wrap\" href=\"index.php?page=newsmanager/comments&func=status&id=###id###\"><span class=\"rex-online\"><i class=\"rex-icon rex-icon-online\"></i> ".rex_i18n::msg("comment_online")."</span></a>";
-else
-$str = "<a class=\"no-wrap\" href=\"index.php?page=newsmanager/comments&func=status&id=###id###\"><span class=\"rex-offline\"><i class=\"rex-icon rex-icon-offline\"></i> ".rex_i18n::msg("comment_offline")."</span></a>";
-return $str;'
-        )
-    );
-    
+
+    $list->setColumnFormat('status', 'custom', static function ($params)
+    {
+        global $I18N;
+        $list = $params["list"];
+        if ($list->getValue("status") == 1) {
+            $str = "<a class=\"no-wrap\" href=\"index.php?page=newsmanager/comments&func=status&id=###id###\"><span class=\"rex-online\"><i class=\"rex-icon rex-icon-online\"></i> " . rex_i18n::msg("comment_online") . "</span></a>";
+        }
+        else {
+            $str = "<a class=\"no-wrap\" href=\"index.php?page=newsmanager/comments&func=status&id=###id###\"><span class=\"rex-offline\"><i class=\"rex-icon rex-icon-offline\"></i> " . rex_i18n::msg("comment_offline") . "</span></a>";
+        }
+        return $str;
+    });
+
     $list->setColumnLabel('status', rex_i18n::msg('comment_tableheader_status'));
     $list->setColumnSortable('status');
-    
+
     $list->addColumn('deleteComment', '', -1, ['<th></th>', '<td class="rex-table-action">###VALUE###</td>']);
     $list->setColumnParams('deleteComment', ['id' => '###id###', 'func' => 'delete']);
     $list->addLinkAttribute('deleteComment', 'data-confirm', rex_i18n::msg('newsmanager_delete_question') . ' ?');
-    $list->setColumnFormat('deleteComment', 'custom', function ($params) {
+    $list->setColumnFormat('deleteComment', 'custom', function ($params)
+    {
         $list = $params['list'];
         return $list->getColumnLink('deleteComment', '<i class="rex-icon rex-icon-delete"></i> ' . rex_i18n::msg('newsmanager_delete'));
     });
-    
+
     $content .= $list->get();
-} elseif ($func == 'edit') {
-    
+}
+elseif ($func == 'edit') {
+
     $user = '';
     $headline = $this->i18n('comment_title_fragment');
 
     $form = rex_form::factory(rex::getTablePrefix() . 'newsmanager_comments', '', 'id = ' . $id);
-    
+
     $result = $form->getSql();
- 
+
     $article_id = $result->getArrayValue('re_article_pid');
 
     $article_result = rex_sql::factory();
     //$article_result->setDebug();
-    $article_result->getArray('SELECT `title` FROM ' . rex::getTablePrefix() . 'newsmanager WHERE `pid` = '. $article_id );
-    
+    $article_result->getArray('SELECT `title` FROM ' . rex::getTablePrefix() . 'newsmanager WHERE `pid` = ' . $article_id);
+
     $headline .= ': "' . $article_result->getArray()[0]['title'] . '"';
-    
+
     $fragment->setVar('title', $headline, false);
-    
+
     $form->addParam('id', $id);
-    
+
     $field = $form->addReadOnlyTextField('createdate');
     $field->setLabel(rex_i18n::msg('comment_label_createdate'));
-    
+
     $field = $form->addTextField('name');
     $field->setLabel(rex_i18n::msg('comment_label_name'));
-    
+
     $field = $form->addTextField('email');
     $field->setLabel(rex_i18n::msg('comment_label_email'));
-    
+
     $field = $form->addTextAreaField('comment');
     $field->setLabel(rex_i18n::msg('comment_label_comment'));
-    
+
     $form->getMessage();
     $content .= $form->get();
-    
+
     $fragment->setVar('class', 'edit');
 }
 
